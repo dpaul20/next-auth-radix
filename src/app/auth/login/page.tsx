562122d628e78f87e9e3f874ccc96ca9ecae2651
Deivy <dpaul_20@hotmail.com>
2024-03-26 15:52:51 -0300

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@radix-ui/themes";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Button>Login</Button>
+    </div>
+  );
+}
+
+export default LoginPage;
